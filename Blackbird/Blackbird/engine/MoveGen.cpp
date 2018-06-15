0f7e3b53e87180a41b8645c825c481387ae2d6b5
mathias-dietrich <mathias.dietrich@gmail.com>
2018-06-15 09:01:20 -0700

    move gen visibale but not implemented

---
+++ Blackbird/Blackbird/engine/MoveGen.cpp
@@ -0,0 +1,9 @@
+//
+//  MoveGen.cpp
+//  Blackbird
+//
+//  Created by Mathias Dietrich on 6/15/18.
+//  Copyright © 2018 Mathias Dietrich. All rights reserved.
+//
+
+#include "MoveGen.hpp"
---
+++ Blackbird/Blackbird/engine/MoveGen.hpp
@@ -0,0 +1,14 @@
+//
+//  MoveGen.hpp
+//  Blackbird
+//
+//  Created by Mathias Dietrich on 6/15/18.
+//  Copyright © 2018 Mathias Dietrich. All rights reserved.
+//
+
+#ifndef MoveGen_hpp
+#define MoveGen_hpp
+
+#include <stdio.h>
+
+#endif /* MoveGen_hpp */
